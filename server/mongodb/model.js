4aa7fc91d47ef43fcf4fb4f8eba3f5e2b3d73980
xuyangyang2019 <15268296911@163.com>
2020-01-09 09:51:55 +0800

    导入path模块

--- server/mongodb/model.js
+++ server/mongodb/model.js
@@ -1,5 +1,6 @@
 // scan all models defined in models:
 const fs = require('fs')
+const path = require('path')
 
 const files = fs.readdirSync(path.resolve(__dirname, '../models'))
 const js_files = files.filter(f => {
@@ -13,7 +14,8 @@ module.exports = {}
  * 自动导入models下的所有js
  */
 for (const f of js_files) {
-    console.log(`import model from file ${f}...`)
+    // console.log(`import model from file ${f}...`)
     const name = f.substring(0, f.length - 3)
+    // console.log(name)
     module.exports[name] = require(path.resolve(__dirname, '../models', f))
 }
