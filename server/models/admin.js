472bed55c7a7eb3c6a1c3d222665fa5b8955b5b0
xuyangyang2019 <15268296911@163.com>
2020-01-09 11:16:47 +0800

    删除冗余代码

--- server/models/admin.js
+++ server/models/admin.js
@@ -1,6 +1,3 @@
-// const mongoose = require('../mongoose')
-// const Schema = mongoose.Schema
-// const Promise = require('bluebird')
 const db = require('../mongodb/db')
 
 module.exports = db.defineModel('Admin', {
