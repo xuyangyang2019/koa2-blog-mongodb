d82e607f40210271aa089d988b9e1b000f20b41f
xuyangyang2019 <15268296911@163.com>
2020-09-15 11:18:28 +0800

    搭建新的框架

--- server/models/admin.js
+++
@@ -1,7 +0,0 @@
-const db = require('../mongodb/db')
-
-module.exports = db.defineModel('Admin', {
-    username: String,
-    email: String,
-    password: String
-})
--- server/models/category.js
+++
@@ -1,7 +0,0 @@
-const db = require('../mongodb/db')
-
-module.exports = db.defineModel('Category', {
-    cate_name: String,
-    cate_order: String,
-    cate_num: Number
-})
