3bc621deacfe3bc67786c5d4ce3e193b58a2e055
xuyangyang2019 <15268296911@163.com>
2020-09-29 12:00:34 +0800

    添加注释，并且同步路由状态(route state)到 store

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
-import Vue from 'vue';
-import App from './App.vue';
-import createRouter from './router';
-import createStore from './store/store.js';
+import Vue from 'vue'
+import App from './App.vue'
+import createRouter from './router'
+import createStore from './store/store.js'
+import { sync } from 'vuex-router-sync'
 
 export function createApp() {
-    const store = createStore();
-    const router = createRouter();
+    // 创建 router 和 store 实例
+    const store = createStore()
+    const router = createRouter()
 
+    // 同步路由状态(route state)到 store
+    sync(store, router)
+
+    // 创建应用程序实例，将 router 和 store 注入
     const app = new Vue({
         router,
         store,
         render: h => h(App)
-    });
+    })
 
-    return { app, store, router, App };
+    // 暴露 app, router 和 store。
+    return { app, store, router, App }
 }
