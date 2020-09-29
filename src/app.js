9705858777f36cc78b5fcd83d3bd9b818a08cf6c
xuyangyang2019 <15268296911@163.com>
2020-09-29 15:18:18 +0800

    路由组件重用时，调用 asyncData 函数

--- src/app.js
+++ src/app.js
@@ -4,6 +4,37 @@ import createRouter from './router'
 import createStore from './store/store.js'
 import { sync } from 'vuex-router-sync'
 
+// 匹配要渲染的视图后，再获取数据
+// Vue.mixin({
+//     beforeMount() {
+//         const { asyncData } = this.$options
+//         if (asyncData) {
+//             // 将获取数据操作分配给 promise
+//             // 以便在组件中，我们可以在数据准备就绪后
+//             // 通过运行 `this.dataPromise.then(...)` 来执行其他任务
+//             this.dataPromise = asyncData({
+//                 store: this.$store,
+//                 route: this.$route
+//             })
+//         }
+//     }
+// })
+
+// 当路由组件重用时，也应该调用 asyncData 函数
+Vue.mixin({
+    beforeRouteUpdate(to, from, next) {
+        const { asyncData } = this.$options
+        if (asyncData) {
+            asyncData({
+                store: this.$store,
+                route: to
+            }).then(next).catch(next)
+        } else {
+            next()
+        }
+    }
+})
+
 export function createApp() {
     // 创建 router 和 store 实例
     const store = createStore()
