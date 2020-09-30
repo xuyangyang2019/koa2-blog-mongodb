d5340b88ae2586d8cb4d446cb9264d9f45127e31
xuyangyang2019 <15268296911@163.com>
2020-09-30 21:36:06 +0800

    解决服务端渲染进不去的bug

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs')
 const path = require('path')
 
@@ -7,60 +6,82 @@ const Router = require('koa-router')
 const serve = require('koa-static')
 const { createBundleRenderer } = require('vue-server-renderer')
 
+// 后端Server
 const backendApp = new Koa()
 const backendRouter = new Router()
 
+// server.bundle.js demo
+// const bundle = fs.readFileSync(path.resolve(__dirname, '../dist/server.bundle.js'), 'utf-8')
+// const renderer = require('vue-server-renderer').createBundleRenderer(bundle, {
+//     template: fs.readFileSync(path.resolve(__dirname, '../dist/index.ssr.html'), 'utf-8')
+// })
+
+// 使用serverBundle和clientManifest进行优化
 const serverBundle = require(path.resolve(__dirname, '../dist/vue-ssr-server-bundle.json'))
 const clientManifest = require(path.resolve(__dirname, '../dist/vue-ssr-client-manifest.json'))
 const template = fs.readFileSync(path.resolve(__dirname, '../dist/index.ssr.html'), 'utf-8')
 
 const renderer = createBundleRenderer(serverBundle, {
-    runInNewContext: false,  // 推荐
+    runInNewContext: false, // 推荐
     template: template, // （可选）页面模板
     clientManifest: clientManifest // （可选）客户端构建 manifest
 })
 
-// 后端Server
-backendApp.use(serve(path.resolve(__dirname, '../dist')))
-backendRouter.get('*', (ctx, next) => {
-    // console.log('ctx', ctx)
-    // console.log('url', ctx.url)
+// 解析静态资源
+// backendApp.use(serve(path.resolve(__dirname, '../dist')))
 
+// 路由
+backendRouter.get('/*', async (ctx, next) => {
+    console.log(ctx.url)
     const context = { url: ctx.url }
     // 这里无需传入一个应用程序，因为在执行 bundle 时已经自动创建过。
     // 现在我们的服务器与应用程序已经解耦！
-    // renderer.renderToString(context, (err, html) => {
-    //     // 处理异常……
-    //     if (err) {
-    //         console.log(err)
-    //         // ctx.status = 500
-    //         // ctx.type = 'html'
-    //         // ctx.body = err
-    //     }
-    //     ctx.status = 200
-    //     ctx.type = 'html'
-    //     ctx.body = html
-    // })
+    try {
+        // vuex demo
+        // let html = await new Promise((resolve, reject) => {
+        //     // 这里直接使用 renderToString 的 Promise 模式，返回的 html 字符串没有样式和 __INITIAL_STATE__，原因暂时还没有查到
+        //     // 所以，只能暂时先自己封装一个 Promise，用 renderToString 的 callback 模式
+        //     renderer.renderToString((err, html) => {
+        //         if (err) {
+        //             reject(err)
+        //         } else {
+        //             resolve(html)
+        //         }
+        //     })
+        // })
 
-    // 流式传输
-    // 如果你依赖由组件生命周期钩子函数填充的上下文数据，则不建议使用流式传输模式
-    const stream = renderer.renderToStream(context)
-    let html = ''
-    stream.on('data', data => {
-        html += data.toString()
-    })
-
-    stream.on('end', () => {
-        // console.log(html) // 渲染完成
-        ctx.status = 200
+        // vuex router demo
+        let html = await renderer.renderToString(context)
+        console.log(html)
         ctx.type = 'html'
+        ctx.status = 200
         ctx.body = html
-    })
 
-    stream.on('error', err => {
-        // handle error...
-        console.log(err)
-    })
+        // 流式传输
+        // 如果你依赖由组件生命周期钩子函数填充的上下文数据，则不建议使用流式传输模式
+        // const stream = renderer.renderToStream(context)
+        // let html = ''
+        // stream.on('data', data => {
+        //     html += data.toString()
+        // })
+
+        // stream.on('end', () => {
+        //     // console.log(html) // 渲染完成
+        //     ctx.status = 200
+        //     ctx.type = 'html'
+        //     ctx.body = html
+        // })
+
+        // stream.on('error', err => {
+        //     // handle error...
+        //     console.log(err)
+        // })
+
+    } catch (err) {
+        console.error(err)
+        ctx.status = 500
+        ctx.body = '服务器内部错误'
+    }
 })
 
 backendApp
