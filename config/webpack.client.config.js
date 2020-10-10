b1eaed34e7503a0d0cd5488daee246ae415ad7cf
xuyangyang2019 <15268296911@163.com>
2020-10-10 11:59:33 +0800

    添加注释

--- config/webpack.client.config.js
+++ config/webpack.client.config.js
@@ -1,19 +1,21 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
-const base = require('./webpack.base.config');
+const path = require('path')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.config')
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = merge(base, {
+module.exports = merge(baseConfig, {
     entry: {
         client: path.resolve(__dirname, '../src/entry-client.js')
     },
 
     plugins: [
+        // 此插件在输出目录中,生成 `vue-ssr-client-manifest.json`。
         new VueSSRClientPlugin(),
+        // 此插件在输出目录中,生成 index.html,用来演示spa
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../src/index.html'),
             filename: 'index.html'
         })
     ]
-});
+})
