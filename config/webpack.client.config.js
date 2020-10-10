5456e3f3ad77ba438aa1d41ee61a365c508f676d
xuyangyang2019 <15268296911@163.com>
2020-10-10 15:26:41 +0800

    splitChunks

--- config/webpack.client.config.js
+++ config/webpack.client.config.js
@@ -8,7 +8,14 @@ module.exports = merge(baseConfig, {
     entry: {
         client: path.resolve(__dirname, '../src/entry-client.js')
     },
-
+    // 重要信息：这将 webpack 运行时分离到一个引导 chunk 中，
+    // 以便可以在之后正确注入异步 chunk。
+    // 这也为你的 应用程序/vendor 代码提供了更好的缓存。
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
     plugins: [
         // 此插件在输出目录中,生成 `vue-ssr-client-manifest.json`。
         new VueSSRClientPlugin(),
